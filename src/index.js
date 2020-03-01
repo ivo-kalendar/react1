6a364079392f07358c830b14afc01dc9c3c0b537
Ivo <ivokalendar@icloud.com>
2020-03-01 19:58:44 +0100

    Clean Up and start...

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './App.css'
+import { appStyle, btnStyle } from './testStyles'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+	return (
+		<div style={appStyle} className="App">
+		  <h1>Locale Time:</h1>
+		  <h1>{new Date().toLocaleTimeString()}</h1>
+		  <button style={btnStyle}>Click Me!</button>
+		</div>
+	)
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById('root'))
